dd94d356b1dd83ff052522229d40623c5c42d6a9
SoeHtut <soehtut@escapepixel.com>
2018-11-25 20:13:16 +0630

    nested components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import Avengers from "./Avengers";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>My First React App</h1>
+        <Avengers />
       </div>
     );
   }
---
+++ src/Avengers.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Avengers extends Component {
+  render() {
+    return (
+      <div className="avenger">
+        <div>Name: Thor</div>
+        <div>Age: 30</div>
+      </div>
+    );
+  }
+}
+
+export default Avengers;
