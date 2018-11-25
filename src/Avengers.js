b4143850ec8fbfd76b0e599849cf55c8a4922945
SoeHtut <soehtut@escapepixel.com>
2018-11-25 21:12:11 +0630

    stateless or UI or functional component

--- src/Avengers.js
+++ src/Avengers.js
@@ -1,18 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Avengers extends Component {
-  render() {
-    const { avengers } = this.props;
-    const avengerList = avengers.map(avenger => {
-      return (
-        <div className="avenger" key={avenger.id}>
-          <div>Name: {avenger.name}</div>
-          <div>Age: {avenger.age}</div>
-        </div>
-      );
-    });
-    return <div className="avenger-list">{avengerList}</div>;
-  }
-}
+const Avengers = ({ avengers }) => {
+  //   const { avengers } = props;
+  const avengerList = avengers.map(avenger => {
+    return (
+      <div className="avenger" key={avenger.id}>
+        <div>Name: {avenger.name}</div>
+        <div>Age: {avenger.age}</div>
+      </div>
+    );
+  });
+  return <div className="avenger-list">{avengerList}</div>;
+};
 
 export default Avengers;
