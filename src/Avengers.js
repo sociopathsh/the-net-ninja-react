bc9346911ad55c72d5db76b98bd9e6578313f138
SoeHtut <soehtut@escapepixel.com>
2018-11-25 21:26:01 +0630

    conditional output

--- src/Avengers.js
+++ src/Avengers.js
@@ -1,14 +1,27 @@
 import React from "react";
 
+// const Avengers = ({ avengers }) => {
+//   const avengerList = avengers.map(avenger => {
+//     if (avenger.age >= 30) {
+//       return (
+//         <div className="avenger" key={avenger.id}>
+//           <div>Name: {avenger.name}</div>
+//           <div>Age: {avenger.age}</div>
+//         </div>
+//       );
+//     } else {
+//       return null;
+//     }
+//   });
+
 const Avengers = ({ avengers }) => {
-  //   const { avengers } = props;
   const avengerList = avengers.map(avenger => {
-    return (
+    return avenger.age >= 30 ? (
       <div className="avenger" key={avenger.id}>
         <div>Name: {avenger.name}</div>
         <div>Age: {avenger.age}</div>
       </div>
-    );
+    ) : null;
   });
   return <div className="avenger-list">{avengerList}</div>;
 };
